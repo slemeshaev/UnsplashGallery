54ccead35c70d88221ddbaaf0860c4542a310ea7
slemeshaev <lemeshaevs@gmail.com>
2021-02-20 00:41:39 +0300

    favorites view controller

---
+++ UnsplashGallery/UnsplashGallery/FavoritesViewController.swift
@@ -0,0 +1,8 @@
+//
+//  FavoritesViewController.swift
+//  UnsplashGallery
+//
+//  Created by Станислав Лемешаев on 20.02.2021.
+//
+
+import Foundation
---
+++ UnsplashGallery/UnsplashGallery/PhotosCollectionViewController.swift
@@ -0,0 +1,8 @@
+//
+//  PhotosCollectionViewController.swift
+//  UnsplashGallery
+//
+//  Created by Станислав Лемешаев on 19.02.2021.
+//
+
+import Foundation
