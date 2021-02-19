99d32588ccfeb5fda2f220d1f4eb6fad41d40f92
slemeshaev <lemeshaevs@gmail.com>
2021-02-19 23:42:42 +0300

    files for project

---
+++ UnsplashGallery/UnsplashGallery/MainTabbarController.swift
@@ -0,0 +1,8 @@
+//
+//  MainTabbarController.swift
+//  UnsplashGallery
+//
+//  Created by Станислав Лемешаев on 19.02.2021.
+//
+
+import Foundation
---
+++ UnsplashGallery/UnsplashGallery/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UnsplashGallery
+//
+//  Created by Станислав Лемешаев on 19.02.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
