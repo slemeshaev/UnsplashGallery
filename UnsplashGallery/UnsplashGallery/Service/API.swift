51875aea99d20cfc59963b528507628c28b93159
slemeshaev <lemeshaevs@gmail.com>
2021-02-20 22:47:51 +0300

    accesskey

--- UnsplashGallery/UnsplashGallery/Service/API.swift
+++ UnsplashGallery/UnsplashGallery/Service/API.swift
@@ -8,6 +8,7 @@
 import Foundation
 
 struct API {
+    static let accessKey = "iUeJ5-_d_x_hRDvd2ek1ELMszxMudbkQVRfmn5Fsb9c"
     static let scheme = "https"
     static let host = "api.unsplash.com"
     static let path = "/search/photos"
