96a8616b9cbd50cf86474c49c7c589f3ecc45cd2
slemeshaev <lemeshaevs@gmail.com>
2021-02-20 10:59:49 +0300

    two navigations controllers

--- UnsplashGallery/UnsplashGallery/MainTabbarController.swift
+++ UnsplashGallery/UnsplashGallery/MainTabbarController.swift
@@ -9,13 +9,34 @@ import UIKit
 
 class MainTabbarController: UITabBarController {
     
+    // MARK: - Properties
+    
+    // MARK: - Lifecycle
+    
     override func viewDidLoad() {
-        view.backgroundColor = .gray
         let photosVC = PhotosCollectionViewController(collectionViewLayout: UICollectionViewLayout())
         let favoritesVC = FavoritesViewController()
         
-        viewControllers = [photosVC, favoritesVC]
+        let navigationPhotosVC = createNavigationViewController(rootVC: photosVC, title: "Фото", image: createImageIcon(nameImage: "photo.on.rectangle"))
+        let navigationFavoritesVC = createNavigationViewController(rootVC: favoritesVC, title: "Избранное", image: createImageIcon(nameImage: "suit.heart.fill"))
+        
+        viewControllers = [navigationPhotosVC, navigationFavoritesVC]
     }
+    
+    // MARK: - Helpers
+    
+    private func createNavigationViewController(rootVC: UIViewController, title: String, image: UIImage) -> UIViewController {
+        let navigationVC = UINavigationController(rootViewController: rootVC)
+        navigationVC.tabBarItem.title = title
+        navigationVC.tabBarItem.image = image
+        return navigationVC
+    }
+    
+    private func createImageIcon(nameImage: String) -> UIImage {
+        guard let image = UIImage(systemName: nameImage) else { return UIImage() }
+        return image
+    }
+    
     
 }
 
