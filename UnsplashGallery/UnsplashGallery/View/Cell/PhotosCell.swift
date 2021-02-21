a2b60265456d7d4ea88169acf832ce5188646cc9
slemeshaev <lemeshaevs@gmail.com>
2021-02-21 10:58:35 +0300

    photos cell

--- UnsplashGallery/UnsplashGallery/View/Cell/PhotosCell.swift
+++ UnsplashGallery/UnsplashGallery/View/Cell/PhotosCell.swift
@@ -5,4 +5,18 @@
 //  Created by Станислав Лемешаев on 21.02.2021.
 //
 
-import Foundation
+import UIKit
+
+class PhotosCell: UICollectionViewCell {
+    
+    static let reuseId = "PhotosCell"
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
