6bac601bc3ff19169051a005df259ea8a2e1cf66
slemeshaev <lemeshaevs@gmail.com>
2021-02-22 17:04:33 +0300

    delete background color

--- UnsplashGallery/UnsplashGallery/View/Cell/PhotosCell.swift
+++ UnsplashGallery/UnsplashGallery/View/Cell/PhotosCell.swift
@@ -26,7 +26,6 @@ class PhotosCell: UICollectionViewCell {
     let photoImageView: UIImageView = {
         let imageView = UIImageView()
         imageView.translatesAutoresizingMaskIntoConstraints = false
-        imageView.backgroundColor = #colorLiteral(red: 0.3647058904, green: 0.06666667014, blue: 0.9686274529, alpha: 1)
         imageView.contentMode = .scaleAspectFill
         return imageView
     }()
