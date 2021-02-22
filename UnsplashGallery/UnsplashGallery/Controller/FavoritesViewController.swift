1eaa169b0cbe5d5d461ce68f32cfe84600aa127f
slemeshaev <lemeshaevs@gmail.com>
2021-02-22 15:45:28 +0300

    favorites controller

--- UnsplashGallery/UnsplashGallery/Controller/FavoritesViewController.swift
+++ UnsplashGallery/UnsplashGallery/Controller/FavoritesViewController.swift
@@ -9,8 +9,77 @@ import UIKit
 
 class FavoritesViewController: UICollectionViewController {
     
+    // MARK: - Properties
+    
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "ФОТОГРАФИИ"
+        label.font = UIFont.systemFont(ofSize: 15, weight: .medium)
+        label.textColor = #colorLiteral(red: 0.501960814, green: 0.501960814, blue: 0.501960814, alpha: 1)
+        return label
+    }()
+    
+    private let enterSearchTermLabel: UILabel = {
+        let label = UILabel()
+        label.text = "У вас еще нет добавленных фотографий..."
+        label.numberOfLines = 0
+        label.textAlignment = .center
+        label.textColor = .gray
+        label.font = UIFont.boldSystemFont(ofSize: 20)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    private var photos = [UnsplashPhoto]()
+    
+    // MARK: - Lifecycle
+    
     override func viewDidLoad() {
-        view.backgroundColor = .green
+        setupCollectionView()
+        setupEnterLabel()
+        setupNavigationBar()
+    }
+    
+    // MARK: - Overrides functions
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        enterSearchTermLabel.isHidden = photos.count != 0
+        return photos.count
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: FavoritesCell.reuseId, for: indexPath) as! FavoritesCell
+        let unsplashPhoto = photos[indexPath.item]
+        cell.unsplashPhoto = unsplashPhoto
+        return cell
+    }
+    
+    // MARK: - Actions
+    
+    // MARK: - Helpers
+    
+    private func setupCollectionView() {
+        collectionView.backgroundColor = .white
+        collectionView.register(FavoritesCell.self, forCellWithReuseIdentifier: FavoritesCell.reuseId)
+        collectionView.contentInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+        
+        let layout = collectionView.collectionViewLayout as! UICollectionViewFlowLayout
+        layout.minimumInteritemSpacing = 1
+        layout.minimumLineSpacing = 1
+    }
+    
+    private func setupEnterLabel() {
+        collectionView.addSubview(enterSearchTermLabel)
+        enterSearchTermLabel.centerXAnchor.constraint(equalTo: collectionView.centerXAnchor).isActive = true
+        enterSearchTermLabel.topAnchor.constraint(equalTo: collectionView.topAnchor, constant: 50).isActive = true
+        enterSearchTermLabel.leadingAnchor.constraint(equalTo: collectionView.leadingAnchor, constant: 15).isActive = true
+        enterSearchTermLabel.trailingAnchor.constraint(equalTo: collectionView.trailingAnchor, constant: -15).isActive = true
+    }
+    
+    private func setupNavigationBar() {
+        navigationItem.leftBarButtonItem = UIBarButtonItem.init(customView: titleLabel)
+        //navigationItem.rightBarButtonItem = trashBarButtonItem
+        //trashBarButtonItem.isEnabled = false
     }
     
 }
